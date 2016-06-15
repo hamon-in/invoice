011bdafbccb542167aba5a3fc409dac3ef0c6e88
Noufal Ibrahim <noufal@nibrahim.net.in>
2016-06-15 15:29:15 +0530

    Adds helper to read from edited file.

--- invoice/helpers.py
+++ invoice/helpers.py
@@ -1,4 +1,8 @@
 import functools
+import os
+import subprocess
+import sys
+import tempfile
 
 def memoise(fn):
     fn.cache = {}
@@ -12,3 +16,24 @@ def memoise(fn):
             fn.cache[key] = val
         return val
     return memoised_fn
+
+
+def get_from_file(init_string=''):
+    f = tempfile.NamedTemporaryFile(delete = False)
+    fname = f.name
+    f.write(init_string.encode("utf-8"))
+    f.close()
+    editor_cmd = os.environ.get("EDITOR", "/usr/bin/vi").split()
+    editor_cmd.append(f.name)
+    p = subprocess.Popen(editor_cmd)
+    p.wait()
+    if p.returncode != 0:
+        raise IOError("Error while spawning editor")
+    with open(f.name) as f0:
+        data = f0.read()
+    os.unlink(f.name)
+    return data
+    
+
+
+    
