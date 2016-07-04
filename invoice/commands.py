afa1141a02092e1e54c63e086fbb4bd443652bc8
Noufal Ibrahim <noufal@nibrahim.net.in>
2016-07-05 00:20:43 +0530

    Adds db_check so that init isn't checked.

--- invoice/commands.py
+++ invoice/commands.py
@@ -20,18 +20,23 @@ class Command:
         self.args = ChainMap(args.__dict__, envars_config)
         self.l = logging.getLogger("invoice")
         self.formatters = formatters.get_formatters()
+        try:
+            self.db_check
+        except AttributeError:
+            self.db_check = True
         
         # Check database version
-        sess = model.get_session(self.args['db'])
-        try:
-            db_version = sess.query(model.Config).filter(model.Config.name == "version").one().value
-            if db_version != __version__:
-                self.l.critical("Database version is %s. Software version is %s. Can't proceed.", db_version, __version__)
-                raise TypeError("Database version mismatch")
-            self.l.info("Database version is '%s'", db_version)
-        except NoResultFound:
-            self.l.critical("No version found in database. Cannot proceed.")
-            raise
+        if self.db_check:
+            sess = model.get_session(self.args['db'])
+            try:
+                db_version = sess.query(model.Config).filter(model.Config.name == "version").one().value
+                if db_version != __version__:
+                    self.l.critical("Database version is %s. Software version is %s. Can't proceed.", db_version, __version__)
+                    raise TypeError("Database version mismatch")
+                self.l.info("Database version is '%s'", db_version)
+            except NoResultFound:
+                self.l.critical("No version found in database. Cannot proceed.")
+                raise
         
 
     def __call__(self):
@@ -48,6 +53,7 @@ class Command:
 
 class InitCommand(Command):
     def __init__(self, args):
+        self.db_check = False
         super().__init__(args)
 
     def __call__(self):
