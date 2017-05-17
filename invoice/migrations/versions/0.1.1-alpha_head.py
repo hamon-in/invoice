acb8e278bfbf2389db5a725e0dc2b9063ebc34c8
Noufal Ibrahim <noufal@hamon.in>
2017-05-17 11:40:27 +0530

    Null database upgrade

---
+++ invoice/migrations/versions/0.1.1-alpha_head.py
@@ -0,0 +1,23 @@
+"""head
+
+Revision ID: 0.1.1-alpha
+Revises: 0.1.0-alpha
+Create Date: 2017-05-17 11:40:04.964638
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '0.1.1-alpha'
+down_revision = '0.1.0-alpha'
+branch_labels = None
+depends_on = None
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
