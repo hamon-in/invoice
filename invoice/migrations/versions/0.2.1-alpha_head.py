cccc45a6487821655f82b13e5b545a091e7ba877
Noufal Ibrahim <noufal@hamon.in>
2017-07-13 13:45:18 +0530

    Bump version to 0.2.1-alpha

---
+++ invoice/migrations/versions/0.2.1-alpha_head.py
@@ -0,0 +1,23 @@
+"""head
+
+Revision ID: 0.2.1-alpha
+Revises: 0.1.1-alpha
+Create Date: 2017-07-13 13:43:20.960992
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '0.2.1-alpha'
+down_revision = '0.1.1-alpha'
+branch_labels = None
+depends_on = None
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
